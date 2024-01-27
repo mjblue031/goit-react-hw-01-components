1dee50b100d17f2fd6d47a74fad145e3e284bdb2
mjblue031 <mjbluerosales@gmail.com>
2024-01-27 22:17:27 +0800

    update package.json

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.number.isRequired })
+  ).isRequired,
+};
